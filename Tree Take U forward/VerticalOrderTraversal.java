cc803cc3b51168a837e498ea03c21b94cba07b84
YadneshKhode <yadneshkhode@gmail.com>
2022-10-18 16:36:38 +0530

    vertical order traversal

--- Tree Take U forward/VerticalOrderTraversal.java
+++ Tree Take U forward/VerticalOrderTraversal.java
@@ -3,6 +3,88 @@ import java.util.*;
 //https://www.youtube.com/watch?v=q_a6lpbKJdw&list=PLJ_vPQ_vraNz90tiB1HNgUWjivW07RcXC&index=18
 //https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 
+/**
+ * 
+ * Yadnesh CODE
+ */
+
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        TreeMap<Integer, PriorityQueue<Pair>> map = new TreeMap<>();
+        ArrayDeque<Pairx> deque = new ArrayDeque<>();
+        deque.add(new Pairx(root, 0, 0));
+
+        while (!deque.isEmpty()) {
+            Pairx curr = deque.removeFirst();
+            if (!map.containsKey(curr.vertical)) {
+                map.put(curr.vertical, new PriorityQueue<>(new PairComparator()));
+            }
+            map.get(curr.vertical).add(new Pair(curr.level, curr.node.val));
+            if (curr.node.left != null) {
+                deque.addLast(new Pairx(curr.node.left, curr.vertical - 1, curr.level + 1));
+            }
+            if (curr.node.right != null) {
+                deque.addLast(new Pairx(curr.node.right, curr.vertical + 1, curr.level + 1));
+            }
+        }
+
+        for (PriorityQueue<Pair> pq : map.values()) {
+            ArrayList<Integer> currList = new ArrayList<>();
+            while (!pq.isEmpty()) {
+                currList.add(pq.remove().nodeVal);
+            }
+            result.add(currList);
+        }
+        return result;
+    }
+
+    class Pair {
+        int level;
+        int nodeVal;
+
+        Pair(int level, int nodeVal) {
+            this.level = level;
+            this.nodeVal = nodeVal;
+        }
+    }
+
+    class PairComparator implements Comparator<Pair> {
+        public int compare(Pair a, Pair b) {
+            if (a.level > b.level)
+                return 1;
+            else if (a.level < b.level)
+                return -1;
+            else {
+                if (a.nodeVal > b.nodeVal)
+                    return 1;
+                else if (a.nodeVal < b.nodeVal)
+                    return -1;
+                return 0;
+            }
+        }
+    }
+
+    class Pairx {
+        TreeNode node;
+        int vertical;
+        int level;
+
+        public Pairx(TreeNode node, int vertical, int level) {
+            this.node = node;
+            this.vertical = vertical;
+            this.level = level;
+        }
+    }
+}
+
+/**
+ * 
+ * STRIVER CODE
+ */
+
 public class VerticalOrderTraversal {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> list = new ArrayList<>();
