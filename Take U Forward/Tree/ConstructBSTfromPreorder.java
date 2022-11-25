1261e752653138fb88e18c3fb11c28dc3330d200
YadneshKhode <yadneshkhode@gmail.com>
2022-11-26 00:57:56 +0530

    construct BST from preorder

--- Take U Forward/Tree/ConstructBSTfromPreorder.java
+++ Take U Forward/Tree/ConstructBSTfromPreorder.java
@@ -1,5 +1,7 @@
-import java.util.*;;
+import java.util.*;
 
+//https://www.youtube.com/watch?v=UmJT3j26t1I&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=55
+//https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
 public class ConstructBSTfromPreorder {
     public TreeNode bstFromPreorderNSquareTC(int[] preorder) {
         /*
@@ -69,6 +71,42 @@ public class ConstructBSTfromPreorder {
                 inorderEnd,
                 indexMap);
 
+        return root;
+    }
+
+    /*
+     * 
+     * 1. When we insert our first node the upper bound is Integer.MAX_VALUE.
+     * 2. Now we select 2nd element in the array if we have to place it on the left
+     * side of the first node then the max value that 2nd element can have is just
+     * less than value of 1st node since this is a BST
+     * 3. But if 2nd element is greater than first node then we have to place it on
+     * right hand side and now the
+     * value can be anything ( < integer.max_value which is current upperbound)
+     * 4. we are deep in tree and we want to place new element now to place it on
+     * left of curr node it has to be less then curr node so we pass upper bound as
+     * curr node and if it is greater than curr node then we pass upper bound same
+     * as the upper bound of the parent of curr node since this value cannot be
+     * greater than the parent of curr node
+     * 5. If both conditions are not met then we have to place this node to the
+     * right of curr node's parent's node, again if the condition fails then we have
+     * to place it to the right of parent's parent node and so on, until we reach
+     * node where upper bound is Integer.max_value and we can place it on its right
+     * side
+     * 
+     * 
+     */
+
+    public TreeNode bstFromPreorderMostOptimised(int[] preorder) {
+        return bstFromPreorderMostOptimised(preorder, Integer.MAX_VALUE, new int[] { 0 });
+    }
+
+    public TreeNode bstFromPreorderMostOptimised(int[] preorder, int upperBound, int[] index) {
+        if (index[0] == preorder.length || preorder[index[0]] > upperBound)
+            return null;
+        TreeNode root = new TreeNode(preorder[index[0]++]);
+        root.left = bstFromPreorderMostOptimised(preorder, root.val, index);
+        root.right = bstFromPreorderMostOptimised(preorder, upperBound, index);
         return root;
     }
 }
