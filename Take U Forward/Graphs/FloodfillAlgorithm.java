462abe2e3fc6c883160b8ebf5ddd2323e4959074
YadneshKhode <yadneshkhode@gmail.com>
2022-12-19 18:56:36 +0530

    added dfs approach in flood fill

--- Take U Forward/Graphs/FloodfillAlgorithm.java
+++ Take U Forward/Graphs/FloodfillAlgorithm.java
@@ -1,3 +1,4 @@
+//https://takeuforward.org/graph/flood-fill-algorithm-graphs/
 public class FloodfillAlgorithm {
     public int[][] floodFillRecursiveMethod(int[][] image, int sr, int sc, int newColor) {
         boolean[][] vis = new boolean[image.length][image[0].length];
@@ -24,5 +25,41 @@ public class FloodfillAlgorithm {
         floodFillRecur(image, sr + 1, sc, newColor, vis, startPixel);
         floodFillRecur(image, sr, sc + 1, newColor, vis, startPixel);
         floodFillRecur(image, sr, sc - 1, newColor, vis, startPixel);
+    }
+
+    public int[][] floodFillDFS(int[][] image, int sr, int sc, int newColor) {
+        int delRow[] = { -1, 0, +1, 0 };
+        int delCol[] = { 0, +1, 0, -1 };
+        dfs(image, sr, sc, newColor, image[sr][sc], delRow, delCol);
+
+        return image;
+    }
+
+    /*
+     * Time Complexity: O(NxM + NxMx4) ~ O(N x M)
+     * 
+     * For the worst case, all of the pixels will have the same colour, so DFS
+     * function will be called for (N x M) nodes and for every node we are
+     * traversing for 4 neighbours, so it will take O(N x M x 4) time.
+     * 
+     * 
+     * Space Complexity: O(N x M) + O(N x M)
+     * 
+     * O(N x M) for copied input array(we edited the input which is bad practice
+     * ideally we should have created a copy of images array and returned that as
+     * answer) and recursive stack space takes up N x M
+     * locations at max.
+     */
+    public void dfs(int[][] image, int sr, int sc, int newColor, int startPixel, int delRow[], int delCol[]) {
+        image[sr][sc] = newColor;
+        for (int i = 0; i < 4; i++) {
+            int nrow = sr + delRow[i];
+            int ncol = sc + delCol[i];
+            if (nrow >= 0 && nrow < image.length && ncol >= 0 && ncol < image[0].length
+                    && image[nrow][ncol] == startPixel && image[nrow][ncol] != newColor) {
+                dfs(image, nrow, ncol, newColor, startPixel, delRow, delCol);
+            }
+        }
+
     }
 }
